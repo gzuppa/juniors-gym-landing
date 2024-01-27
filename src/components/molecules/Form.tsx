cf599c5a9b912df83cea3dfd05649a95f3393572
gzuppa <gzuppa86@gmail.com>
2024-01-26 20:30:49 -0600

    fix: caluclator styling

--- src/components/molecules/Form.tsx
+++ src/components/molecules/Form.tsx
@@ -19,13 +19,13 @@ const Form = () => {
 
             let message = '';
             if (Number(bmi) < 18.5) {
-                message = 'You are Underweight';
+                message = 'Te encuentras debajo de tu peso';
             } else if (Number(bmi) >= 18.5 && Number(bmi) < 25) {
-                message = 'You are Normal weight';
+                message = 'Tu peso está en el índice adecuado';
             } else if (Number(bmi) >= 25 && Number(bmi) < 30) {
-                message = 'You are Overweight';
+                message = 'Tienes sobrepeso';
             } else {
-                message = 'You are Obese';
+                message = 'Cuidado, tienes obesidad';
             }
             setBmiMessage(message);
         } else {
@@ -37,29 +37,29 @@ const Form = () => {
         <main className="w-full grid gap-6 pb-10 md:pb-0">
 
             <InputField className="w-full flex flex-col gap-2">
-                <label htmlFor="height" className="uppercase text-sm text-zinc-200 font-bold">Height</label>
+                <label htmlFor="height" className="uppercase text-sm text-zinc-200 font-bold">Altura</label>
                 <div className="w-full relative">
-                    <input type="number" placeholder="Enter your height" className="w-full h-12 pl-4 pr-8 text-zinc-400 outline-none transition-all duration-200 focus:border-amber-500 border border-zinc-400 bg-transparent" value={heightValue} onChange={(event: React.ChangeEvent<HTMLInputElement>) => setHeightValue(event.target.value)} />
+                    <input type="number" placeholder="Ingresa tu altura en centímetros" className="w-full h-12 pl-4 pr-8 text-zinc-400 outline-none transition-all duration-200 focus:border-amber-500 border border-zinc-400 bg-transparent" value={heightValue} onChange={(event: React.ChangeEvent<HTMLInputElement>) => setHeightValue(event.target.value)} />
                     <Text as="span" className="absolute text-zinc-200 font-bold top-3 right-4">cm</Text>
                 </div>
             </InputField>
 
             <InputField className="w-full flex flex-col gap-2">
-                <label htmlFor="weight" className="uppercase text-sm text-zinc-200 font-bold">Weight</label>
+                <label htmlFor="weight" className="uppercase text-sm text-zinc-200 font-bold">Peso</label>
                 <div className="w-full relative">
-                    <input type="number" placeholder="Enter your weight" className="w-full h-12 pl-4 pr-8 text-zinc-400 outline-none transition-all duration-200 focus:border-amber-500 border border-zinc-400 bg-transparent" value={weightValue} onChange={(event: React.ChangeEvent<HTMLInputElement>) => setWeightValue(event.target.value)} />
+                    <input type="number" placeholder="Ingresa tu peso en kilos" className="w-full h-12 pl-4 pr-8 text-zinc-400 outline-none transition-all duration-200 focus:border-amber-500 border border-zinc-400 bg-transparent" value={weightValue} onChange={(event: React.ChangeEvent<HTMLInputElement>) => setWeightValue(event.target.value)} />
                     <Text as="span" className="absolute text-zinc-200 font-bold top-3 right-4">kg</Text>
                 </div>
             </InputField>
 
             <div className="w-full mt-4">
-                <Button onClick={calculateBmi} type="button" className="px-6 py-3 bg-gradient-to-r from-red-500 to-amber-500 text-zinc-200 text-sm uppercase font-semibold">Calculate Now</Button>
+                <Button onClick={calculateBmi} type="button" className="px-6 py-3 bg-gradient-to-r from-purple-800 to-purple-300 text-zinc-200 text-sm uppercase font-semibold">Calcular</Button>
             </div>
 
             {
                 bmiValue && <div className="w-full flex flex-col p-4 bg-zinc-700">
-                    <Text as="h2" className="text-zinc-200 text-lg">Your BMI is <span className="font-extrabold">{bmiValue}</span></Text>
-                    <Text as="p" className="text-amber-500">{bmiMessage}</Text>
+                    <Text as="h2" className="text-zinc-200 text-lg">Tu IMC es <span className="font-extrabold">{bmiValue}</span></Text>
+                    <Text as="p" className="text-yellow-300">{bmiMessage}</Text>
                 </div>
             }
 
